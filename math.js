16b0ab93a7727da80a6ef9c1d84df148e8ed8f6a
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2022-06-09 16:36:22 -0300

    up math pronto

--- math.js
+++ math.js
@@ -1,15 +1,19 @@
 function soma(a, b) {
+    //soma
     return a+b
 }
 function subtracao(a, b) {
+    //subtrai
     return a-b
 }
 
 function divisao(a, b) {
+    //divide
     return a/b
 }
 
 function multiplicacao(a, b) {
+    //mutiplica
     return a*b
 }
 
